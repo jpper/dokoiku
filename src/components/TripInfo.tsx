a0b445a51f294a9046a5f4aa5acc30121c661f28
Diallo <diallo.spears@gmail.com>
2019-12-04 16:41:38 +0900

    Committing before attempting to refactor store

--- src/components/TripInfo.tsx
+++ src/components/TripInfo.tsx
@@ -13,6 +13,7 @@ export const TripInfo: React.FC<TripInfoProps> = props => {
       <p>End Date: </p>
       <p>Locations: </p>
       <p>Budget: </p>
+      <p>Notes: </p>
       <p>Members: </p>
     </div>
   );
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,3 +1,33 @@
-import { combineReducers } from "redux";
+import { combineReducers, createStore } from "redux";
+
+//This is a work in progress
+
+const initialState = {
+  trips: [],
+  currentTrip: 0,
+  showProfile: false,
+  showChat: false
+};
+
+type Action = {
+  type: string;
+};
+
+const reducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case "PRINT": {
+      console.log("HOWDY");
+      return {
+        trips: [...state.trips],
+        currentTrip: state.currentTrip,
+        showProfile: state.showProfile,
+        showChat: state.showChat
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 export const rootReducer = combineReducers({});
