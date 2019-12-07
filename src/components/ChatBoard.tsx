2681287d9027430c574d6a674fe3248ebbe6a924
Imamachi-n <naoto.imamachi@gmail.com>
2019-12-07 15:00:33 +0900

    created the basic chatroom

--- src/components/ChatBoard.tsx
+++ src/components/ChatBoard.tsx
@@ -3,38 +3,22 @@ import uuidv4 from "uuid/v4";
 import firebase, { firestore } from "firebase";
 import React, { Component } from "react";
 // import ReactLoading from "react-loading";
-import { myFirebase, myFirestore } from "../config/firebase";
+import { myFirestore } from "../config/firebase";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { setMessages } from "../redux/action";
 
-// TODO: for mui test
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+// Material UI & Styles
+import {
+  Container,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  Card
+} from "@material-ui/core";
 import "../styles/ChatBoard.css";
-
-// const useStyles = makeStyles(() => {
-//   createStyles({
-//       card: {
-//         minWidth: 275,
-//       },
-//       bullet: {
-//         display: 'inline-block',
-//         margin: '0 2px',
-//         transform: 'scale(0.8)',
-//       },
-//       title: {
-//         fontSize: 14,
-//       },
-//       pos: {
-//         marginBottom: 12,
-//       },
-//   })
-// });
 
 const mapStateToProps = (state: any) => ({
   userId: state.userId,
@@ -113,200 +97,77 @@ class ChatBoard extends Component<Props, ChatBoardState> {
     };
   }
   async componentDidMount() {
-    // Get memberIds
     await this.props.getMessages(this.props.tripId);
-    //await this.getGroupMemberInfo(this.state.currentUserId);
-    // console.log(this.state.currentPeerUserIds);
-    // await this.state.currentPeerUserIds.forEach(id => {
-    //   this.getGroupMemberInfo(id);
-    //   console.log("GET GROUP MEM INFO: ", id);
-    // });
-    // await this.getListHistory();
-    // this.addNameToMsg();
   }
-  // getListHistory = () => {
-  //   const messageListener = null;
-  //   myFirestore
-  //     .collection("trips")
-  //     .doc(this.state.tripId)
-  //     .collection("messages")
-  //     .onSnapshot(
-  //       snapShot => {
-  //         snapShot.docChanges().forEach(change => {
-  //           if (change.type === "added") {
-  //             this.setState({
-  //               listMessage: [...this.state.listMessage, change.doc.data()]
-  //             });
-  //           }
-  //         });
-  //       },
-  //       err => {
-  //         console.log(err.toString());
-  //       }
-  //     );
-  //   this.setState({
-  //     messageListener
-  //   });
-
-  //   // // TODO:
-  //   // this.addNameToMsg();
-  // };
-  // getGroupMembers = () => {
-  //   const groupMemberListener = null;
-
-  //   myFirestore
-  //     .collection("trips")
-  //     .doc(this.state.tripId)
-  //     .onSnapshot(snapShot => {
-  //       console.log(snapShot.data());
-  //       this.setState({
-  //         currentPeerUserIds: snapShot.data().memberIds
-  //       });
-  //       console.log("GET GROUP MEMEBERS!");
-  //       console.log(this.state.currentPeerUserIds);
-
-  //       this.state.currentPeerUserIds.forEach(id => {
-  //         this.getGroupMemberInfo(id);
-  //         console.log("GET GROUP MEM INFO: ", id);
-  //       });
-  //     });
-  // };
-  // sendMessage = () => {
-  //   const content = this.state.message;
-  //   const moment = firestore.FieldValue.serverTimestamp();
-  //   const message = {
-  //     content,
-  //     fromId: this.state.currentUserId,
-  //     moment
-  //   };
-  //   myFirestore
-  //     .collection("trips")
-  //     .doc(this.state.tripId)
-  //     .collection("messages")
-  //     .doc(uuidv4())
-  //     .set(message)
-  //     .then(() => {
-  //       console.log("successful!");
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // };
 
   handleChange(e: React.FormEvent<HTMLInputElement>) {
     this.setState({
       messageToBeSent: e.currentTarget.value
     });
   }
-  // getGroupMemberInfo(id: string) {
-  //   myFirestore
-  //     .collection("users")
-  //     .where("id", "==", id)
-  //     .get()
-  //     .then(snapshot => {
-  //       if (snapshot.empty) {
-  //         console.log("No matching user");
-  //         return;
-  //       }
-  //       const tmpInfo: any[] = [];
-  //       snapshot.forEach((doc: any) => {
-  //         tmpInfo.push(doc.data());
-  //       });
-  //       console.log(tmpInfo);
-  //       this.setState({
-  //         groupMemberInfo: tmpInfo
-  //       });
-  //     })
-  //     .then(() => {
-  //       console.log("GET GROUP MEM INFO!!!!");
-  //       console.log(this.state.groupMemberInfo);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }
-  // addNameToMsg() {
-  //   const tmpList = this.state.listMessage.map(msg => {
-  //     const fromId = msg.fromId;
-  //     const fromName = this.state.groupMemberInfo.filter((info: any) => {
-  //       return info.id === fromId;
-  //     })[0].nickname;
-  //     msg.fromName = fromName;
-  //     return msg;
-  //   });
-  //   this.setState({ listMessage: tmpList });
-  //   console.log(this.state.listMessage);
-  // }
-  render() {
-    // const classes = useStyles({});
-    const bull = <span className="bullet">•</span>;
 
+  clearMessage() {
+    this.setState({
+      messageToBeSent: ""
+    });
+  }
+
+  render() {
     return (
       <div className="ChatBoard">
-        <Card>
-          <CardContent>test</CardContent>
-        </Card>
+        <Container>
+          <Card className="card">
+            <List>
+              {this.props.tripMessages.length ? (
+                this.props.tripMessages
+                  .sort((a: any, b: any) => a.moment.seconds - b.moment.seconds)
+                  .map((msg: any) => (
+                    <>
+                      <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                          <Avatar alt="Remy Sharp" src={msg.photoUrl} />
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={msg.content}
+                          secondary={
+                            <React.Fragment>
+                              <div>{msg.nickname}</div>
+                              <div>{moment(msg.moment.toDate()).fromNow()}</div>
+                            </React.Fragment>
+                          }
+                        />
+                      </ListItem>
+                    </>
+                  ))
+              ) : (
+                <div></div>
+              )}
+            </List>
+          </Card>
 
-        <Card className="card">
-          <CardContent>
-            <Typography className="title" color="textSecondary" gutterBottom>
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="h2">
-              be
-              {bull}
-              nev
-              {bull}o{bull}
-              lent
-            </Typography>
-            <Typography className="pos" color="textSecondary">
-              adjective
-            </Typography>
-            <Typography variant="body2" component="p">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-
-        {this.props.tripMessages.length ? (
-          this.props.tripMessages
-            .sort((a: any, b: any) => a.moment.seconds - b.moment.seconds)
-            .map((msg: any) => (
-              <div>
-                <div>{moment(msg.moment.toDate()).fromNow()}</div>
-                <div>{msg.nickname}</div>
-                <img src={msg.photoUrl} />
-                <div>{msg.content}</div>
-              </div>
-            ))
-        ) : (
-          <div></div>
-        )}
-        <form>
-          <label htmlFor="message"></label>
-          <input
-            type="text"
-            id="message"
-            onChange={e => this.handleChange(e)}
-          ></input>
-          <button
-            type="button"
-            onClick={() =>
-              this.props.sendMessage(
-                this.props.tripId,
-                this.props.userId,
-                this.state.messageToBeSent
-              )
-            }
-          >
-            Send Message!
-          </button>
-        </form>
+          <form>
+            <label htmlFor="message"></label>
+            <input
+              type="text"
+              id="message"
+              value={this.state.messageToBeSent}
+              onChange={e => this.handleChange(e)}
+            ></input>
+            <button
+              type="button"
+              onClick={() => {
+                this.clearMessage();
+                this.props.sendMessage(
+                  this.props.tripId,
+                  this.props.userId,
+                  this.state.messageToBeSent
+                );
+              }}
+            >
+              Send Message!
+            </button>
+          </form>
+        </Container>
       </div>
     );
   }
