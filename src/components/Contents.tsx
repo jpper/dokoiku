8a4e7c95f391232f0cac5830fdf38fe5634f9ffd
Imamachi-n <naoto.imamachi@gmail.com>
2019-12-10 11:51:26 +0900

    Fixed and merged the notes

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -292,20 +292,18 @@ class Contents extends React.Component<myProps, any> {
                   </Grid>
                   {/* {if statement and changing props value here} */}
                   <Grid item xs={7}>
-                    {this.props.ongoingTrips.length ? (
-                      <Map
-                        trips={this.props.ongoingTrips}
-                        currentTripIndex={this.props.currentOngoingTripIndex}
-                      />
-                    ) : null}
-                    {/* {this.props.mapTripMessage === 1 &&
-                      // <Editor />
-                      // <ChatBoard />
-                    } */}
+                    {this.props.ongoingTrips.length &&
+                      this.props.mapTripMessage === 0 && (
+                        <Map
+                          trips={this.props.ongoingTrips}
+                          currentTripIndex={this.props.currentOngoingTripIndex}
+                        />
+                      )}
+                    {this.props.mapTripMessage === 1 && (
+                      <Editor tripId="testNotes" />
+                    )}
 
-                    {/* {this.props.mapTripMessage === 2 &&
-                      // <ChatBoard />
-                    } */}
+                    {this.props.mapTripMessage === 2 && <ChatBoard />}
                   </Grid>
                 </Grid>
               </>
