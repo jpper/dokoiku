fc69e812742b40142de7103f3b79c4b33f2c484f
Imamachi-n <naoto.imamachi@gmail.com>
2019-12-22 23:06:45 +0900

    Created the cloud function for firestore backup

---
+++ gcp-functions/functions/README.md
@@ -0,0 +1,37 @@
+# Cloud Functions for Firebase (FireStore Backup)
+
+## Setup Cloud Functions
+
+```bash
+firebase init functions
+```
+
+```bash
+firebase deploy --only functions:backupFirestoreToStorage
+
+gcloud projects add-iam-policy-binding project-gopherdor \
+--member serviceAccount:project-gopherdor@appspot.gserviceaccount.com \
+--role roles/datastore.importExportAdmin
+
+gsutil iam ch serviceAccount:project-gopherdor@appspot.gserviceaccount.com:admin \
+gs://gopherdor-firestore-backup
+```
+
+## Manual export & import fireStore data
+
+### Export
+
+```bash
+gcloud config set project project-gopherdor
+gcloud firestore export gs://gopherdor-firestore-backup
+```
+
+### Import
+
+```bash
+gcloud firestore import gs://gopherdor-firestore-backup/2019-12-22T12:07:48_23056/
+```
+
+## Reference
+
+https://firebase.google.com/docs/firestore/manage-data/export-import
---
+++ gcp-functions/functions/index.js
@@ -0,0 +1,64 @@
+const { region } = require("firebase-functions");
+const { credential } = require("firebase-admin");
+const axios = require("axios");
+
+const GCLOUD_REGION = "asia-east2";
+const GCLOUD_TIMEZONE = "Asia/Tokyo";
+const GCLOUD_PROJECT_ID = "project-gopherdor";
+const GCLOUD_BACKET_NAME = "gopherdor-firestore-backup";
+
+module.exports.backupFirestoreToStorage = region(GCLOUD_REGION)
+  .pubsub.schedule("0 3 * * *")
+  .timeZone(GCLOUD_TIMEZONE)
+  .onRun(async () => {
+    try {
+      const accessToken = await credential
+        .applicationDefault()
+        .getAccessToken()
+        .then(result => result.access_token);
+      const response = await axios.post(
+        `https://firestore.googleapis.com/v1/projects/${GCLOUD_PROJECT_ID}/databases/(default):exportDocuments`,
+        { outputUriPrefix: `gs://${GCLOUD_BACKET_NAME}` },
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+// const functions = require("firebase-functions");
+// const firestore = require("@google-cloud/firestore");
+// const client = new firestore.v1.FirestoreAdminClient();
+
+// const GCLOUD_REGION = "asia-east2";
+// const GCLOUD_TIMEZONE = "Asia/Tokyo";
+// const GCLOUD_PROJECT_ID = "project-gopherdor";
+// const GCLOUD_BACKET_NAME = "gs://gopherdor-firestore-backup";
+
+// exports.scheduledFirestoreExport = functions
+//   .region(GCLOUD_REGION)
+//   .pubsub.schedule("0 3 * * *")
+//   .timeZone(GCLOUD_TIMEZONE)
+//   .onRun(context => {
+//     const databaseName = client.databasePath(GCLOUD_PROJECT_ID, "(default)");
+
+//     return client
+//       .exportDocuments({
+//         name: databaseName,
+//         outputUriPrefix: GCLOUD_BACKET_NAME,
+//         // Leave collectionIds empty to export all collections
+//         // or set to a list of collection IDs to export,
+//         // collectionIds: ['users', 'posts']
+//         collectionIds: []
+//       })
+//       .then(responses => {
+//         const response = responses[0];
+//         console.log(`Operation Name: ${response["name"]}`);
+//         return response;
+//       })
+//       .catch(err => {
+//         console.error(err);
+//         throw new Error("Export operation failed");
+//       });
+//   });
